a41fb2f59b6594b8de8e72d230996b944866970a
Sagar G Darji <42430138+darjisagar7@users.noreply.github.com>
2025-03-14 17:06:33 +0530

    Create SynchronizedMethod.java

---
+++ PrintFooBarAlternatively/SynchronizedMethod.java
@@ -0,0 +1,72 @@
+//https://leetcode.com/problems/print-foobar-alternately
+
+package practice;
+
+import java.util.concurrent.ExecutorService;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Synchronized sp = new Synchronized(10);
+        ExecutorService executorService = newFixedThreadPool(3);
+
+        executorService.submit(() -> {
+            try {
+                sp.foo(() -> System.out.println("Foo"));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        executorService.submit(() -> {
+            try {
+                sp.bar(() -> System.out.println("Bar"));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        executorService.shutdown();
+    }
+}
+
+
+package practice;
+
+public class Synchronized {
+    private int n;
+    // 0 = Foo
+    // 1 = Bar
+    private int flag = 0;
+
+    public Synchronized(int n) {
+        this.n = n;
+    }
+
+    public synchronized void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            while (flag == 1) {
+                wait();
+            }
+            printFoo.run();
+            flag = 1;
+            notify();
+        }
+    }
+
+    public synchronized void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            while (flag == 0) {
+                wait();
+            }
+            printBar.run();
+            flag = 0;
+            notify();
+        }
+    }
+}
