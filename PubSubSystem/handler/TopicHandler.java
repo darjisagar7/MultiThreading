2a4c98a792f9dd1fc081e64bbba1c3ef5402eb21
Sagar G Darji <42430138+darjisagar7@users.noreply.github.com>
2025-03-15 16:26:38 +0530

    Create TopicHandler.java

---
+++ PubSubSystem/handler/TopicHandler.java
@@ -0,0 +1,31 @@
+package practice.pubsub;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TopicHandler {
+    private final Topic topic;
+    private final Map<String, SubscriberWorker> subscriberWorkers;
+
+    public TopicHandler(Topic topic) {
+        this.topic = topic;
+        subscriberWorkers = new HashMap<>();
+    }
+
+    public void publish() {
+        for (TopicSubscriber topicSubscriber:topic.getSubscribers()) {
+            startSubScriberWorker(topicSubscriber);
+        }
+    }
+
+    public void startSubScriberWorker(final TopicSubscriber topicSubscriber) {
+        final String subscriberId = topicSubscriber.getSubscriber().getId();
+        if (!subscriberWorkers.containsKey(subscriberId)) {
+            final SubscriberWorker subscriberWorker = new SubscriberWorker(topic, topicSubscriber);
+            subscriberWorkers.put(subscriberId, subscriberWorker);
+            new Thread(subscriberWorker).start();
+        }
+        final SubscriberWorker subscriberWorker = subscriberWorkers.get(subscriberId);
+        subscriberWorker.wakeUpIfNeeded();
+    }
+}
